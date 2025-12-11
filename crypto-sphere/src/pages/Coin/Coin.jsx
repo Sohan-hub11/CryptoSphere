4632a0fb8523c3195329092d477e4d5b310ecb64
Sohan Samanta <sohansamanta18@gmail.com>
2025-12-12 01:50:15 +0530

    v1

---
+++ crypto-sphere/src/pages/Coin/Coin.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Coin.css'
+
+const Coin = () => {
+  return (
+    <div>
+      <center>
+        <h2>This is Coin Page</h2>
+      </center>
+    </div>
+  )
+}
+import './Coin.css'
+
+export default Coin
---
+++ crypto-sphere/src/pages/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Home.css'
+
+const Home = () => {
+  return (
+    <div>
+      <center>
+        <h2>This is Home Page</h2>
+      </center>
+    </div>
+  )
+}
+import './Home.css'
+
+export default Home
