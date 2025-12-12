0d497a3fe35f9e545b6654bd123dd9ef12960d5c
Sohan Samanta <sohansamanta18@gmail.com>
2025-12-12 22:04:01 +0530

    v2

--- crypto-sphere/src/pages/Home/Home.jsx
+++ crypto-sphere/src/pages/Home/Home.jsx
@@ -3,10 +3,24 @@ import './Home.css'
 
 const Home = () => {
   return (
-    <div>
-      <center>
-        <h2>This is Home Page</h2>
-      </center>
+    <div className='home'>
+      <div className='hero'>
+        <h1>Largest <br /> Crypto Marketplace</h1>
+        <p>Welcome to world's largest cryptocurrency marketplace. Sign up to explaore more about cryptos</p>
+        <form>
+          <input type="text" placeholder='Search crypto...' />
+          <button type="submit">Search</button>
+        </form>
+      </div>
+      <div className='crypto-table'>
+        <div className="table-layout">
+          <p>#</p>
+          <p>Coins</p>
+          <p>Price</p>
+          <p style={{ textAlign: "center" }}>24H Change</p>
+          <p className='market-cap'>Market Cap</p>
+        </div>
+      </div>
     </div>
   )
 }
