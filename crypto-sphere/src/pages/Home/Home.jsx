6b8531c845086c9d2494b3c1e42a7b5dcc894fc8
Sohan Samanta <sohansamanta18@gmail.com>
2025-12-14 01:20:38 +0530

    v3

--- crypto-sphere/src/pages/Home/Home.jsx
+++ crypto-sphere/src/pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect, useState } from 'react'
 import './Home.css'
 import { CoinContext } from '../../context/CoinContext'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
 
@@ -33,7 +34,12 @@ const Home = () => {
         <h1>Largest <br /> Crypto Marketplace</h1>
         <p>Welcome to world's largest cryptocurrency marketplace. Sign up to explaore more about cryptos</p>
         <form onSubmit={searchHandler}>
-          <input onChange={inputHandler} value={input} type="text" placeholder='Search crypto...' required />
+          <input onChange={inputHandler} value={input} list='coinlist' type="text" placeholder='Search crypto...' required />
+
+          <datalist id='coinlist'>
+            {allCoin.map((item, index) => (<option key={index} value={item.name} />))}
+          </datalist>
+
           <button type="submit">Search</button>
         </form>
       </div>
@@ -47,7 +53,7 @@ const Home = () => {
         </div>
         {
           displayCoin.slice(0, 10).map((item, index) => (
-            <div className="table-layout" key={index}>
+            <Link to={`/coin/${item.id}`} className="table-layout" key={index}>
               <p>{item.market_cap_rank}</p>
               <div>
                 <img src={item.image} alt="" />
@@ -58,7 +64,7 @@ const Home = () => {
                 {Math.floor(item.price_change_percentage_24h * 100) / 100}
               </p>
               <p className='market-cap'>{currency.symbol} {item.market_cap.toLocaleString()}</p>
-            </div>
+            </Link>
 
           ))
         }
